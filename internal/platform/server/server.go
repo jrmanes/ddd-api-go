789b87e86233bcfbaa3dccb0a97d43f63a7b975b
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-02-28 17:08:26 +0100

    add graceful shutdown and ctx

--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -1,8 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jrmanes/ddd-api-go/internal/platform/server/handler/courses"
@@ -14,29 +19,62 @@ type Server struct {
 	httpAddr string
 	engine   *gin.Engine
 
+	shutdownTimeout time.Duration
+
 	// dependencies
 	commandBus command.Bus
 }
 
 // New initialization server Gin
-func New(host string, port uint, commandBus command.Bus) Server {
+func New(ctx context.Context, host string, port uint, shutdownTimeout time.Duration, commandBus command.Bus) (context.Context, Server) {
 	srv := Server{
 		engine:   gin.New(),
 		httpAddr: fmt.Sprintf("%s:%d", host, port),
+
+		shutdownTimeout: shutdownTimeout,
 
 		commandBus: commandBus,
 	}
 
 	srv.registerRoutes()
-	return srv
+	return serverContext(ctx), srv
 }
 
-func (s *Server) Run() error {
-	log.Println("Server running on", s.httpAddr)
-	return s.engine.Run(s.httpAddr)
-}
-
+// Routes definition
 func (s *Server) registerRoutes() {
 	s.engine.GET("/health", health.CheckHandler())
 	s.engine.POST("/courses", courses.CreateHandler(s.commandBus))
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	log.Println("Server running on", s.httpAddr)
+
+	srv := &http.Server{
+		Addr:    s.httpAddr,
+		Handler: s.engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("server shutdown", err)
+		}
+	}()
+
+	<-ctx.Done()
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return srv.Shutdown(ctxShutDown)
+}
+
+func serverContext(ctx context.Context) context.Context {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		<-c
+		cancel()
+	}()
+
+	return ctx
 }
