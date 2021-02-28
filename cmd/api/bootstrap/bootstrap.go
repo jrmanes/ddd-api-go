3cd75783df69bd1d5a0f1de510d43ea5d83f6f5e
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-02-28 17:33:21 +0100

    add db timeouts in queries

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -1,38 +1,50 @@
 package bootstrap
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jrmanes/ddd-api-go/internal/creating"
+	"github.com/jrmanes/ddd-api-go/internal/platform/bus/inmemory"
 	"github.com/jrmanes/ddd-api-go/internal/platform/server"
 	"github.com/jrmanes/ddd-api-go/internal/platform/storage/mysql"
 )
 
 const (
-	host = "localhost"
-	port = 8080
+	host            = "localhost"
+	port            = 8080
+	shutdownTimeout = 10 * time.Second
 
-	dbUser = "api-go"
-	dbPass = "api-go"
-	dbHost = "localhost"
-	dbPort = "3306"
-	dbName = "api-go"
+	dbUser    = "api-go"
+	dbPass    = "api-go"
+	dbHost    = "localhost"
+	dbPort    = "3306"
+	dbName    = "api-go"
+	dbTimeout = 5 * time.Second
 )
 
 // Run initialize the server, if st goes wrong, return an error to main
 func Run() error {
-	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser,dbUser, dbPass, dbHost, dbPort, dbName)
+	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := sql.Open("mysql", mysqlURI)
 	if err != nil {
 		return err
 	}
 
-	courseRepository := mysql.NewCourseRepository(db)
+	var (
+		commandBus = inmemory.NewCommandBus()
+	)
+
+	courseRepository := mysql.NewCourseRepository(db, dbTimeout)
 
 	creatingCourseService := creating.NewCourseService(courseRepository)
 
-	srv := server.New(host, port, creatingCourseService)
-	return srv.Run()
+	createCourseCommandHandler := creating.NewCourseCommandHandler(creatingCourseService)
+	commandBus.Register(creating.CourseCommandType, createCourseCommandHandler)
+
+	ctx, srv := server.New(context.Background(), host, port, shutdownTimeout, commandBus)
+	return srv.Run(ctx)
 }
